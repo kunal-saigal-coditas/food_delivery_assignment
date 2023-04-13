024ba9d0c3e903ec1478746f460a56592b58193d
kunal.saigal-coditas <kunal.saigal@coditas.com>
2023-04-13 12:47:57 +0530

    web ready

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -25,11 +25,6 @@ class HomePage extends StatelessWidget {
         ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          drawer: Drawer(
-            child: Image.asset(
-              'assets/images/pic9.png',
-            ),
-          ),
           appBar: AppBar(
             backgroundColor: Colors.transparent,
             title: Image.asset('assets/images/pic7.png'),
@@ -42,66 +37,68 @@ class HomePage extends StatelessWidget {
           ),
           body: Padding(
             padding: const EdgeInsets.symmetric(vertical: 35, horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Text(
-                  "Hello, John!",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                      fontWeight: FontWeight.normal),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  DateFormat('EEEE, MMM dd, yyyy').format(_currentDate),
-                  style: const TextStyle(color: Colors.white),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                CustomGrid(),
-                const SizedBox(
-                  height: 100,
-                ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  child: SizedBox(
-                    width: 200,
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        padding:
-                            MaterialStateProperty.all(const EdgeInsets.all(8)),
-                        // elevation: MaterialStateProperty.all(6),
-                        backgroundColor: MaterialStateProperty.all(
-                          const Color(0XFF6FDEC2),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Text(
+                    "Hello, John!",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.normal),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    DateFormat('EEEE, MMM dd, yyyy').format(_currentDate),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomGrid(),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    child: SizedBox(
+                      width: 200,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.all(8)),
+                          // elevation: MaterialStateProperty.all(6),
+                          backgroundColor: MaterialStateProperty.all(
+                            const Color(0XFF6FDEC2),
+                          ),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                          ),
                         ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => FirstPage()));
+                        },
+                        child: const Text(
+                          "Next",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
                           ),
                         ),
                       ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => FirstPage()));
-                      },
-                      child: const Text(
-                        "Next",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                        ),
-                      ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
